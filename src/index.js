ef65f6420281d0a548b0bdf2000660b1d600cf1e
Ramnath Shenoy B <bekalshenoy@gmail.com>
2021-02-23 09:48:32 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
+const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const client = new MongoClient(
@@ -9,11 +10,22 @@ const client = new MongoClient(
   { useUnifiedTopology: true }
 );
 let questions;
+let transporter;
 
 async function run() {
   try {
     await client.connect();
-    questions = client.db("cluster0").collection("questions");
+    questions = await client.db("cluster0").collection("questions");
+
+    transporter = await nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: yes,
+      auth: {
+        user: "gcekcse2020",
+        pass: process.env.PASS,
+      },
+    });
   } catch (err) {
     console.log(err);
   }
@@ -28,30 +40,58 @@ app.get("/", (req, res) => {
   res.send("welcome");
 });
 
-app.get("/get_solved", async (req, res) => {
-  questions.find({ status: "solved" }).toArray((err, items) => {
-    if (err) {
-      res.send({ status: "false" });
-    }
+app.post("/get_solved", async (req, res) => {
+  if (req.body.pass == "donotshare") {
+    questions
+      .find({
+        status: "solved",
+        topic: req.body.topic,
+        module: req.body.module,
+      })
+      .toArray((err, items) => {
+        if (err) {
+          res.send({ status: "false" });
+        }
 
-    res.send(items);
-  });
+        res.send(items);
+      });
+  } else {
+    res.status(404).send({ status: "false" });
+  }
 });
 
-app.get("/get_unsolved", async (req, res) => {
-  questions.find({ status: "unsolved" }).toArray((err, items) => {
-    if (err) {
-      res.send({ status: "false" });
-    }
+app.post("/get_unsolved", async (req, res) => {
+  if (req.body.pass == "donotshare") {
+    questions
+      .find({
+        status: "unsolved",
+        topic: req.body.topic,
+        module: req.body.module,
+      })
+      .toArray((err, items) => {
+        if (err) {
+          res.send({
+            status: "false",
+          });
+        }
 
-    res.send(items);
-  });
+        res.send(items);
+      });
+  } else {
+    res.status(404).send({ status: "false" });
+  }
 });
 
 app.post("/set", async (req, res) => {
   if (req.body.pass == "donotshare") {
     questions.insertOne(
-      { question: req.body.question, answer: "", status: "unsolved" },
+      {
+        question: req.body.question,
+        answer: "",
+        status: "unsolved",
+        email: req.body.email,
+        aemail: "",
+      },
       (err, result) => {
         if (err) {
           res.send({ status: "false" });
@@ -69,7 +109,57 @@ app.post("/update", async (req, res) => {
   if (req.body.pass == "donotshare") {
     questions.updateOne(
       { question: req.body.question },
-      { $set: { answer: req.body.answer, status: "solved" } },
+      {
+        $set: {
+          answer: req.body.answer,
+          status: "solved",
+          aemail: req.body.aemail,
+        },
+      },
+      (err, item) => {
+        if (err) {
+          res.send({ status: "false" });
+        } else {
+          transporter.sendMail({
+            from: "gcekcse2020@gmail.com>",
+            to: item.email,
+            subject: "Your Doubt Is Solved",
+            text:
+              "Questions: " + item.question + "\n" + "Answer: " + item.answer,
+          });
+          res.send({ status: "true" });
+        }
+      }
+    );
+  } else {
+    res.status(404).send({ status: "false" });
+  }
+});
+
+app.post("/wrong", async (req, res) => {
+  if (req.body.pass == "donotshare") {
+    transporter.sendMail({
+      from: "gcekcse2020@gmail.com>",
+      to: "gcekcse2020@gmail.com",
+      subject: "Your Doubt Is Solved",
+      text: req.body.question + "\n" + req.body.email,
+    });
+  } else {
+    res.status(404).send({ status: "false" });
+  }
+});
+
+app.post("/delete_answer", async (req, res) => {
+  if (req.body.pass == "donotshare") {
+    questions.updateOne(
+      { question: req.body.question },
+      {
+        $set: {
+          answer: "",
+          status: "unsolved",
+          aemail: "",
+        },
+      },
       (err, item) => {
         if (err) {
           res.send({ status: "false" });
