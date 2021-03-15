a4aa6151bfbc8411e7a2da136cefa9e418e77534
ramnath <bekalshenoy@gmail.com>
2021-03-15 18:49:23 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -43,10 +43,7 @@ const app = express();
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://gcekcse2020.netlify.app"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
